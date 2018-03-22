51ce0b56cca35e98b035851c36b2e4256bb250c7
Maxime Benoit <maxime@MacBook-Pro-de-Maxime.local>
2018-03-22 16:57:08 +0100

    try list files reader

--- logreader.py
+++ logreader.py
@@ -27,3 +27,30 @@ def read_dir(dir_path, nb_files_max=np.inf):
     print('\n[Sucess] %d files loaded' % (p))
 
     return res
+
+
+"""
+path = '/Users/maxime/Documents/full_log/27102017/'
+
+def list_log_files(path):
+
+    log_list = os.listdir(path)
+
+    df = pd.DataFrame(columns=["hostname", "date"])
+
+    for log in log_list:
+        try:
+            reg = re.match('(.+)\.(\d+)\.log',log)
+            hostname = reg.group(1)
+            date = reg.group(2)
+
+            df = df.append({
+             "hostname": hostname,
+             "date":  date
+              }, ignore_index=True)
+        except:
+            pass
+
+    return df
+
+"""
