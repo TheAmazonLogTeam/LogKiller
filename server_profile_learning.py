e5dd0b16ad4bd2ea64131c6066300a5584b30aba
RavimoShark <Joseph.Assouline1@gmail.com>
2018-04-03 16:29:58 +0200

    test phase 2 done

--- server_profile_learning.py
+++ server_profile_learning.py
@@ -1,39 +1,45 @@
 import times_series_learning as tsl
 import numpy as np
+import time
 import sortedcontainers
 import pandas as pd
 
 
 class ServerProfileLearning(object):
 
-    def __init__(self, data, parameters, distribution):
+    def __init__(self, data, parameters, distribution, distribution_period, level_threshold):
         self.label_number = len(np.unique(data['label'].values))
         self.label = np.unique(data['label'].values)
         self.data = data
-        self.parameters = np.ones((self.label_number+1, 4))*parameters
+        self.parameters = np.ones((self.label_number + 1, 4)) * parameters  # see parameters in times_series_learning
         self.data_prep = None
-        self.hostname = self.data.iloc[1, 0]
+        self.hostname = self.data.iloc[0, 1]
         self.server_profile = dict()
-        self.distribution = distribution
-# sortedcontainers.SortedDict(sortedcontainers.SortedList())
+        self.distribution = distribution  # distribution od distance list same for all servers all clusters
+        self.distribution_period = distribution_period  # distribution period same as distance period
+        self.level_threshold = level_threshold  # level we consider for outliers
+
+    # sortedcontainers.SortedDict(sortedcontainers.SortedList())
 
     def preprocess_data(self):
         self.data_prep = self.data.drop(self.data.columns[1:len(self.data.columns) - 1], axis=1)
         self.data_prep = self.data_prep.groupby(['label'])
 
     def set_profile(self):
-
+        t0 = time.time()
         self.preprocess_data()
-        t = tsl.TimesSeriesLearning(self.parameters[0, :])
-        t.set_profile(self.data['timestamp'])
+        t = tsl.TimesSeriesLearning(self.parameters[0, :],
+                                    self.distribution_period, self.distribution, self.level_threshold)
+        t.set_profile(self.data, True)
         self.server_profile[self.hostname + "_general"] = t
         i = 0
 
         for k, v in self.data_prep:
-            t = tsl.TimesSeriesLearning(self.parameters[i, :])
-            t.set_profile(v)
-            self.server_profile[self.hostname + "_"+str(k)] = t
+            t = tsl.TimesSeriesLearning(self.parameters[i, :],
+                                        self.distribution_period, self.distribution, self.level_threshold,
+                                        self.server_profile[self.hostname + "_general"].learning_week_period )
+            t.set_profile(v, False)
+            self.server_profile[self.hostname + "_" + str(k)] = t
+            print('cluster number ' + str(k) + ' of hostname: ' + self.hostname)
             i += 1
-
-
-
+        print("Learning Server" + self.hostname + " Done in " + str(time.time() - t0))
--- times_series_learning.py
+++ times_series_learning.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from datetime import datetime
 import time
 
+
 # distribution of distances by time step (type day)
 # update if it is not in distribution skew
 # Danger profile on te learning times series do not do  an average ideally
@@ -17,39 +18,56 @@ import time
 # compute the spread between two logs
 
 
-class TimesSeriesLearning (object):
+class TimesSeriesLearning(object):
 
-    def __init__(self, parameters, distribution_period, distribution, level_threshold):
-        self.learning_week_period = 0
-        self.period = parameters[0]
-        self.m_avg_period = parameters[1]
-        self.dist_period = parameters[2]
-        self.dist_radius = parameters[3]
+    def __init__(self, parameters, distribution_period, distribution, level_threshold, lwp=0):
+        self.learning_week_period = lwp # seconds between first and last element
+        self.period = parameters[0]  # Sampling Period
+        self.m_avg_period = parameters[1]  # m_avg_period
+        self.dist_period = parameters[2]   # distance period evaluation
+        self.dist_radius = parameters[3]   # distance window
         self.data_week_avg = None
         self.streaming_start_date = None
         self.start = False
         self.level_threshold = level_threshold
         self.profile = None
         self.quantiles = defaultdict(list)
-        self.distribution_period= distribution_period # in minutes
+        self.distribution_period = distribution_period  # in minutes
         self.distribution = distribution
-        self.max_spread=0
+        self.max_spread = 0
+        self.min_spread = np.inf
+
 
     # Resample
     def get_time_series_rs(self, data):
+        #t0 = time.time()
         data.index = pd.to_datetime(data.timestamp, format='%Y-%m-%d %H:%M:%S')
         self.compute_max_spread(data)
         d_min = data.drop(data.columns[1:], axis=1)
-        d_min = d_min.resample(self.period).count()
+        d_min = d_min.resample(str(self.period)+'min').count()
+        #print('Resample processed in:', time.time()-t0)
         return d_min
 
     # return the max period between over a period
     def compute_max_spread(self, data):
-        self.max_spread = max(self.max_spread, np.max(data.index[1:]-data.index[:-1]))
+        #t0 = time.time()
+        if data.shape[0] > 2:
+            d = (data.index[1:] - data.index[:-1])
+            self.max_spread = max(self.max_spread, np.amax(d.seconds.values))
+            self.min_spread = min(self.min_spread, np.amin(d.microseconds.values))
+        elif data.shape[0] == 2:
+            d = (data.index[0] - data.index[1])
+            self.max_spread = max(self.max_spread, np.amax(d.seconds))
+            self.min_spread = min(self.min_spread, np.amin(d.microseconds))
+        else:
+            self.max_spread = self.learning_week_period
+            self.min_spread = self.learning_week_period
+
+        #print('Max spread processed in:', time.time() - t0)
 
     # Moving average
     def mov_avg(self, data):
-        return data.rolling(window=self.m_avg_period).mean().fillna(method='backfill')
+        return data.rolling(window=int(self.m_avg_period)).mean().fillna(method='backfill')
 
     # Dataframe for grouping per week
     def weekly_average(self, data):
@@ -61,10 +79,13 @@ class TimesSeriesLearning (object):
 
     # Set the profile
 
-    def set_profile(self, data):
-        delta = (data.loc[-1, 'timestamp'] - data.loc[0, 'timestamp']).week
-        self.learning_week_period = delta
-        self.profile = self.weekly_average(self.mov_avg(self.get_time_series_rs(data)))
+    def set_profile(self, data, gen):
+        data_rs = self.get_time_series_rs(data)
+
+        if gen:
+            self.learning_week_period = (data_rs.index[0] - data_rs.index[-1]).seconds
+
+        self.profile = self.weekly_average(self.mov_avg(data_rs))
 
     # Online Mean not necessary as we will plot distribution
     # contiguous data
@@ -91,22 +112,22 @@ class TimesSeriesLearning (object):
         minute = streaming_data.loc[0, 'minutes']
         hour = streaming_data.loc[0, 'hours']
         weekday = streaming_data.loc[0, 'weekday']
-        date = weekday*1440+hour*60+minute
+        date = weekday * 1440 + hour * 60 + minute
 
         d = fastdtw(profile_type.loc[weekday][hour][minute:minute + self.dist_period].values,
                     streaming_data[weekday][hour][minute:minute + self.dist_period].values,
                     self.dist_radius, dist=euclidean)[0]
-        return d,date
+        return d, date
 
     # compute quantiles and see if d belongs to
     # improvement store the quantiles
     def threshold(self, d, ind):
         if self.quantiles[ind] is None:
-            quant = mquantiles(self.distribution[ind],prob=[self.level_threshold, (1-self.level_threshold)])
+            quant = mquantiles(self.distribution[ind], prob=[self.level_threshold, (1 - self.level_threshold)])
         else:
             quant = self.quantiles[ind]
 
-        if d < quant[0] or d > quant[1] :
+        if d < quant[0] or d > quant[1]:
             return False
         else:
             return True
@@ -114,10 +135,8 @@ class TimesSeriesLearning (object):
     # adding or not the distance to the actual distribution
     # frequentist view
     def add_to_dist(self, dist_score, date):
-        ind = (24*60*7) // self.distribution_period
+        ind = (24 * 60 * 7) // self.distribution_period
         if self.threshold(dist_score, ind):
             self.distribution[ind].add(dist_score)
         else:
             print("Alert Anomaly detected")
-
-
